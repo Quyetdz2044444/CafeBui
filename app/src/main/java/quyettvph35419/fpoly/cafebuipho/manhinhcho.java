efddb26833f83b4b02cd0273c7189e30218220e0
Nam <duongdinhnam000@gmail.com>
2023-11-08 20:55:00 +0700

    màn chờ

--- app/src/main/java/quyettvph35419/fpoly/cafebuipho/manhinhcho.java
+++ app/src/main/java/quyettvph35419/fpoly/cafebuipho/manhinhcho.java
@@ -1,7 +1,10 @@
 package quyettvph35419.fpoly.cafebuipho;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class manhinhcho extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class manhinhcho extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manhinhcho);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(manhinhcho.this, MainActivity.class);
+                startActivity(intent);
+            }
+        },3000);
     }
 }
