be6ebe26ed8e0fa261d4f5f762c170841de82c09
DatTranTuan <145355463+DatTranTuan@users.noreply.github.com>
2023-11-14 22:15:16 +0700

    Add database
    Add database

--- app/src/main/java/quyettvph35419/fpoly/cafebuipho/Database/DbHelper.java
+++ app/src/main/java/quyettvph35419/fpoly/cafebuipho/Database/DbHelper.java
@@ -8,7 +8,7 @@ import androidx.annotation.Nullable;
 
 public class DbHelper extends SQLiteOpenHelper {
     public static final String DB_NAME = "CafeBui";
-    public static final int DB_VERSION = 6;
+    public static final int DB_VERSION = 7;
 
     public DbHelper(@Nullable Context context) {
         super(context, DB_NAME, null, DB_VERSION);
@@ -29,10 +29,59 @@ public class DbHelper extends SQLiteOpenHelper {
         db.execSQL(createTableKhachHang);
 //--------------------------------------------------------------------------
 //        Bảng đồ uống
+        String createTableLoaiDoUong = "create table LOAIDOUONG(" +
+                "MaLoai integer primary key autoincrement, " +
+                "TenLoai TEXT NOT NULL)";
+        db.execSQL(createTableLoaiDoUong);
+
+        String tb_size = "create table SIZE(" +
+                "MaSize integer primary key autoincrement," +
+                "Size text not null," +
+                "GiaSize integer not null)";
+        db.execSQL(tb_size);
+
+
+        String tb_DoUong= "create table DOUONG(" +
+                "MaDO integer primary key autoincrement," +
+                "TenDO text not null," +
+                "GiaDO integer not null," +
+                "MaSize integer references SIZE(MaSize)," +
+                "Anh integer not null,"
+                +"MaLoai integer references LOAIDOUONG(MaLoai))";
+        db.execSQL(tb_DoUong);
+
+
+        String tb_GioHang= "create table GIOHANG(" +
+                "MaGH integer primary key autoincrement," +
+                "MaDO integer references DOUONG(MaDO)," +
+                "MaSize integer references SIZE(MaSize)," +
+                "SoLuong integer not null,"
+                +"TongTien integer not null)";
+        db.execSQL(tb_GioHang);
+
+
+        String tb_DonHang= "create table DONHANG(" +
+                "MaDH integer primary key autoincrement," +
+                "MAKH Text references KHACHHANG(MAKH)," +
+                "MaDO integer references DOUONG(MaDO)," +
+                "Ngay text not null,"
+                +"Gia integer not null,"
+                +"TrangThai integer not null)";
+        db.execSQL(tb_DonHang);
+
+        String tb_DonHangChiTiet= "create table DONHANGCHITIET(" +
+                "MaDHCT integer primary key autoincrement," +
+                "MaDO integer references DOUONG(MaDO)," +
+                "MaDH integer references DONHANG(MaDH)," +
+                "TongTien integer not null,"
+                +"SoLuong integer not null,"
+                +"Ngay text not null,"
+                +"TrangThai integer not null,"
+                +"MaSize integer references SIZE(MaSize))";
+        db.execSQL(tb_DonHangChiTiet);
 
 
 
-//        ----------------------------------------------------------
 //        INSERT DỮ LIỆU
 
         db.execSQL("INSERT INTO KHACHHANG VALUES('admin','Admin','admin','0983917432','Hà Nam','admin@gmail.com')," +
@@ -45,6 +94,12 @@ public class DbHelper extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         if (oldVersion != newVersion) {
             db.execSQL("drop table if exists KHACHHANG");
+            db.execSQL("drop table if exists LOAIDOUONG");
+            db.execSQL("drop table if exists SIZE");
+            db.execSQL("drop table if exists DOUONG");
+            db.execSQL("drop table if exists GIOHANG");
+            db.execSQL("drop table if exists DONHANG");
+            db.execSQL("drop table if exists DONHANGCHITIET");
             onCreate(db);
         }
     }
