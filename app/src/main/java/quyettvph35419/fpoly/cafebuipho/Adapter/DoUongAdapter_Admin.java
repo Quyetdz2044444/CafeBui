af6174104f99fe87df8b2cf59788fc9c7c89f6cf
Nam <duongdinhnam000@gmail.com>
2023-11-16 19:28:14 +0700

    hiển thị đồ uống admin

---
+++ app/src/main/java/quyettvph35419/fpoly/cafebuipho/Adapter/DoUongAdapter_Admin.java
@@ -0,0 +1,58 @@
+package quyettvph35419.fpoly.cafebuipho.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+
+import quyettvph35419.fpoly.cafebuipho.Fragment.DoUong_Fragment;
+import quyettvph35419.fpoly.cafebuipho.Model.DoUong;
+import quyettvph35419.fpoly.cafebuipho.R;
+
+public class DoUongAdapter_Admin extends ArrayAdapter<DoUong> {
+    private Context context;
+    private ArrayList<DoUong> list;
+    private DoUong_Fragment fragment;
+    private TextView tvid, tvten, tvloai, tvgia;
+    private ImageView imgDoUong;
+
+    public DoUongAdapter_Admin(@NonNull Context context, DoUong_Fragment fragment, ArrayList<DoUong> list) {
+        super(context, 0, list);
+        this.context = context;
+        this.list = list;
+        this.fragment = fragment;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View v = convertView;
+        if (v == null) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            v = inflater.inflate(R.layout.item_douong_admin, null);
+        }
+        final DoUong item = list.get(position);
+        if (item != null) {
+            tvid = v.findViewById(R.id.tvma_doUong);
+            tvten = v.findViewById(R.id.tvten_doUong);
+            tvloai = v.findViewById(R.id.tvloai_doUong);
+            tvgia = v.findViewById(R.id.tvgia_doUong);
+            imgDoUong = v.findViewById(R.id.btnxoa_doUong);
+
+            tvid.setText("Mã Đồ Uống: " + item.getMaLoai());
+            tvten.setText("Tên Đồ Uống: " + item.getTenDoUong());
+            tvloai.setText("Loại Đồ Uống: " + item.getMaLoai());
+            tvgia.setText("Giá: " + item.getGia());
+
+        }
+        return v;
+    }
+}
--- app/src/main/java/quyettvph35419/fpoly/cafebuipho/Fragment/DoUong_Fragment.java
+++ app/src/main/java/quyettvph35419/fpoly/cafebuipho/Fragment/DoUong_Fragment.java
@@ -1,5 +1,6 @@
 package quyettvph35419.fpoly.cafebuipho.Fragment;
 
+import android.app.Dialog;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,31 +8,46 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+
+import quyettvph35419.fpoly.cafebuipho.Adapter.DoUongAdapter_Admin;
+import quyettvph35419.fpoly.cafebuipho.Dao.DoUongDao;
+import quyettvph35419.fpoly.cafebuipho.Model.DoUong;
 import quyettvph35419.fpoly.cafebuipho.R;
 
 
 public class DoUong_Fragment extends Fragment {
+    ListView listView;
+    ArrayList<DoUong> list;
+    static DoUongDao dao;
+    DoUongAdapter_Admin adapter;
+    DoUong item;
+    FloatingActionButton fab;
+    Dialog dialog;
 
 
     public DoUong_Fragment() {
     }
-
-    public static DoUong_Fragment newInstance(String param1, String param2) {
-        DoUong_Fragment fragment = new DoUong_Fragment();
-
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_do_uong, container, false);
+        View v = inflater.inflate(R.layout.fragment_do_uong, container, false);
+        listView = v.findViewById(R.id.lvdouong_doUongAdmin);
+        fab = v.findViewById(R.id.fab_doUongAdmin);
+        dao = new DoUongDao(getActivity());
+        capNhatLv();
+
+        return  v;
     }
+    void capNhatLv() {
+        list = (ArrayList<DoUong>) dao.getAll();
+        adapter = new DoUongAdapter_Admin(getActivity(), this, list);
+        listView.setAdapter(adapter);
+    }
+
+
 }
