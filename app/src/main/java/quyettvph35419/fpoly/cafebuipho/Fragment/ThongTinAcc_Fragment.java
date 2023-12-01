0c051e3d4aff4156d30c3f2a2c51ea0a59b65faa
Nam <duongdinhnam000@gmail.com>
2023-12-01 13:12:49 +0700

    update thông tin khách hàng

--- app/src/main/java/quyettvph35419/fpoly/cafebuipho/Fragment/ThongTinAcc_Fragment.java
+++ app/src/main/java/quyettvph35419/fpoly/cafebuipho/Fragment/ThongTinAcc_Fragment.java
@@ -14,6 +14,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -33,6 +34,7 @@ public class ThongTinAcc_Fragment extends Fragment {
     private KhachHangDao khachHangDao;
     private ArrayList<KhachHang> list;
     private KhachHang khachHang;
+    private Button btnsua;
 
 
     public ThongTinAcc_Fragment() {
@@ -49,6 +51,14 @@ public class ThongTinAcc_Fragment extends Fragment {
         tvdiachi = v.findViewById(R.id.tvdiachi);
         tvsdt = v.findViewById(R.id.tvsdt);
         tvtentk = v.findViewById(R.id.tvTentk);
+
+        btnsua = v.findViewById(R.id.btnSuaUser);
+        btnsua.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+              showUpdateDialog();
+            }
+        });
 
 
         khachHangDao = new KhachHangDao(getContext());
@@ -71,6 +81,82 @@ public class ThongTinAcc_Fragment extends Fragment {
         }
 
         return v;
+    }
+    public void updateUserData() {
+        // Lấy lại dữ liệu từ CSDL hoặc nơi khác
+        khachHang = khachHangDao.getID(khachHang.getmaKH());
+
+        // Cập nhật dữ liệu trên TextView
+        tvtentk.setText("Tên đăng nhập: " + khachHang.getmaKH());
+        tvtendaydu.setText("Họ tên: " + khachHang.getHoTen());
+        tvsdt.setText("Số điện thoại: " + khachHang.getSdt());
+        tvdiachi.setText("Địa chỉ: " + khachHang.getDiaChi());
+        tvemail.setText("Email: " + khachHang.getEmail());
+    }
+
+    private void showUpdateDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(requireContext());
+        View dialogView = getLayoutInflater().inflate(R.layout.dialog_suathongtin_kh, null);
+        builder.setView(dialogView);
+        final AlertDialog dialog = builder.create();
+
+        final EditText edtHoTen = dialogView.findViewById(R.id.edttendaydu_tt);
+        final EditText edtSdt = dialogView.findViewById(R.id.edtsdt_tt);
+        final EditText edtDiaChi = dialogView.findViewById(R.id.edtdiachi_tt);
+        final EditText edtEmail = dialogView.findViewById(R.id.edtemali_tt);
+
+        // Hiển thị thông tin hiện tại trong Dialog
+        edtHoTen.setText(khachHang.getHoTen());
+        edtSdt.setText(khachHang.getSdt());
+        edtDiaChi.setText(khachHang.getDiaChi());
+        edtEmail.setText(khachHang.getEmail());
+
+        Button btnCapNhat = dialogView.findViewById(R.id.btnluu);
+        Button btnhuy = dialogView.findViewById(R.id.btnhuy);
+        btnCapNhat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Thực hiện cập nhật thông tin người dùng
+                String hoTenMoi = edtHoTen.getText().toString();
+                String sdtMoi = edtSdt.getText().toString();
+                String diaChiMoi = edtDiaChi.getText().toString();
+                String emailMoi = edtEmail.getText().toString();
+
+                // Kiểm tra xem có nhập đầy đủ thông tin không (có thể thêm các kiểm tra khác)
+                if (!hoTenMoi.isEmpty() && !sdtMoi.isEmpty() && !diaChiMoi.isEmpty() && !emailMoi.isEmpty()) {
+                    // Thực hiện cập nhật trong CSDL
+                    khachHang.setHoTen(hoTenMoi);
+                    khachHang.setSdt(sdtMoi);
+                    khachHang.setDiaChi(diaChiMoi);
+                    khachHang.setEmail(emailMoi);
+
+                    long result = khachHangDao.updatett(khachHang);
+                    if (result > 0) {
+                        // Cập nhật thành công
+                        // Có thể cập nhật lại thông tin trên TextView hoặc làm gì đó khác
+                        Toast.makeText(requireContext(), "Cập nhật thành công", Toast.LENGTH_SHORT).show();
+                        updateUserData();
+                        dialog.dismiss();
+
+                    } else {
+                        // Cập nhật thất bại
+                        Toast.makeText(requireContext(), "Cập nhật thất bại", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    // Hiển thị thông báo nếu không nhập đủ thông tin
+                    Toast.makeText(requireContext(), "Vui lòng nhập đầy đủ thông tin", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        btnhuy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+
+
+        dialog.show();
     }
 
 }
